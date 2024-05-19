cbe5f3d49f624ab62bd57c8767bfaf3d6e3d2b26
Vidhi Agrawal <163636860+vidhiagrawa@users.noreply.github.com>
2024-05-19 03:24:29 -0700

    Add files via upload

---
+++ shoppers stop/shopper.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("input")
+    setInterval( ()=>{
+      input.placeholder = "Search Product & Brands"
+    } , 2000 )
+    setInterval( ()=>{
+      input.placeholder = "Search Any of the Item"
+    } , 4000 )
+    setInterval( ()=>{
+      input.placeholder = "Search of Youu"
+    } , 6000 )
